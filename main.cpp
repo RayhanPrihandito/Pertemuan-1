c23a8629d45c779c78adb575926f6ef6677e6ad6
Rayhan Prihandito <112876446+RayhanPrihandito@users.noreply.github.com>
2022-09-26 21:12:13 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,38 +4,48 @@ using namespace std;
 
 int main()
 {
-    cout << "Nama : Rayhan Prihandito" << endl;
-    cout << "NIM: A11.2022.14152" << endl;
-    cout << "Mapel: Daspro" << endl;
+    cout << "Nama: Rayhan Prihandito" << "\n";
+    cout << "NIM: A11.2022.14152" << "\n";
+    cout << "Matkul: DASPRO" << endl;
+    cout << "\n";
 
-    //Luas Lingkaran
+    cout << "~~DAFTAR JURUSAN~~" << endl;
+    cout << " 1.Teknik Informatika\n 2.Sistem Informasi\n 3.Desain Komunikasi Visual\n";
+    cout << endl;
 
-    float r, luas;
-    float phi=3.14;
+    int a;
+    cout << "masukkan jurusan anda: ";
+    cin >> a;
 
-    cout << "Masukkan jari-jari: ";
-    cin >> r;
+    if(a==1){
+    cout << "Teknik Informatika"<<endl;
+    }
+    if(a==2){
+    cout << "Sistem Informasi"<<endl;
+    }
+    if(a==3){
+    cout << "Desain Komunikasi Visual"<<endl;
+    }
+    cout <<endl;
 
-    luas=phi*r*r;
-    cout << "luas : " << luas << "\n";
+    cout << "~~KATAGORI MAHASISWA~~" << endl;
+    cout << endl;
 
+    int smt;
 
-    //Persegi Panjang
+    cout << "Semester\n";
+    cout << "1. Semester 1-2 = Freshman\n2. Semester 3-4 = Sophomore\n3. Semester 5-6 = Junior\n4. Semester 7-8 = Senior"<<endl;
+    cout << " Masukan Semester Anda: ";
+    cin >> smt;
+    cout << "\n";
 
-    float P;
-    float L;
-    float T;
-
-    cout << "Masukkan Panjang: ";
-    cin >> P;
-
-    cout << "Masukkan Lebar: ";
-    cin >> L;
-
-    cout << "Masukkan Tinggi: ";
-    cin >> T;
-
-    int Total=P*L*T;
-    cout << "Total: " << Total << "\n";
-    return 0;
+    if (smt == 1) {
+        cout << " Saya dalam kategori Freshman " <<endl;
+    } else if(smt == 2){
+        cout << "Saya dalam kategori Sophomore" <<endl;
+    } else if(smt == 3){
+        cout << "Saya dalam kategori Junior"<<endl;
+    } else if (smt == 4){
+        cout << "Saya dalam kategori Senior"<<endl;
+    }
 }
