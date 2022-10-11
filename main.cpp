faa04875733e63c8f3c4648444ecfd92aaab8978
Rayhan Prihandito <112876446+RayhanPrihandito@users.noreply.github.com>
2022-10-11 16:42:08 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,48 +4,31 @@ using namespace std;
 
 int main()
 {
-    cout << "Nama: Rayhan Prihandito" << "\n";
-    cout << "NIM: A11.2022.14152" << "\n";
-    cout << "Matkul: DASPRO" << endl;
-    cout << "\n";
+    //cout << "Hello world!" << endl;
+    cout << "Nama : Rayhan Prihandito" << endl;
+    cout << "NIM : A11.2022.14152" << endl;
+    cout << "Kelompok : 4104" << endl;
+    cout << "MATKUL : DASPRO" << endl;
+    cout << "Fibonacci" << endl;
+    cout << " " << endl;
 
-    cout << "~~DAFTAR JURUSAN~~" << endl;
-    cout << " 1.Teknik Informatika\n 2.Sistem Informasi\n 3.Desain Komunikasi Visual\n";
-    cout << endl;
+    int t1 = 0, t2 = 1, nextTerm = 0, n;
 
-    int a;
-    cout << "masukkan jurusan anda: ";
-    cin >> a;
+    cout << "input nomor : ";
+    cin >> n;
 
-    if(a==1){
-    cout << "Teknik Informatika"<<endl;
+    cout << "fibonnanci : ";
+
+    nextTerm = t1 + t2;
+
+    while (nextTerm <= n){
+        cout << nextTerm << ", ";
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
     }
-    if(a==2){
-    cout << "Sistem Informasi"<<endl;
-    }
-    if(a==3){
-    cout << "Desain Komunikasi Visual"<<endl;
-    }
-    cout <<endl;
 
-    cout << "~~KATAGORI MAHASISWA~~" << endl;
-    cout << endl;
 
-    int smt;
 
-    cout << "Semester\n";
-    cout << "1. Semester 1-2 = Freshman\n2. Semester 3-4 = Sophomore\n3. Semester 5-6 = Junior\n4. Semester 7-8 = Senior"<<endl;
-    cout << " Masukan Semester Anda: ";
-    cin >> smt;
-    cout << "\n";
-
-    if (smt == 1) {
-        cout << " Saya dalam kategori Freshman " <<endl;
-    } else if(smt == 2){
-        cout << "Saya dalam kategori Sophomore" <<endl;
-    } else if(smt == 3){
-        cout << "Saya dalam kategori Junior"<<endl;
-    } else if (smt == 4){
-        cout << "Saya dalam kategori Senior"<<endl;
-    }
+    return 0;
 }
